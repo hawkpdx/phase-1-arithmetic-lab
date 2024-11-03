b840295f6defbc315558cdc4ca17ba38e90cf1f7
Garrett <hawkpdx@icloud.com>
2024-11-02 17:16:51 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+{
+  // Case 1
+  let num1 = 31;
+  let num2 = 2;
+  let multiply = num1 * num2;
+}
 
-// Write your code here
+{
+  // Case 2
+  let random = Math.ceil(Math.random() * 1000);
+}
+
+{
+  // Case 3
+  let num3 = 9;
+  let num4 = 5;
+  let mod = num3 % num4;
+}
+
+{
+  let max = Math.max(5, 10, 15, 20);
+}
